c1a74f4e58a9a960c20db82618aceb180ee010cc
Arnold217 <arnoldbuining@hotmail.com>
2017-11-04 16:57:51 +0100

    Een bestand gemaakt waar de <head> altijd in zal staan voor iedere pagina

---
+++ php/screens/head.php
@@ -0,0 +1,8 @@
+<head>
+    <!--    Stylesheets     -->
+    <link href="<?=$config['Base_URL'];?>/lib/css/popup.css" rel="stylesheet"/>
+
+    <!--    Javascript      -->
+    <script src="<?=$config['Base_URL'];?>/lib/js/jquery-3.2.1.min.js"></script>
+
+</head>
