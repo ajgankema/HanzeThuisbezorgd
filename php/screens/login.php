e83266c13b4c18fc09508e4ada1b27f69999d926
Arnold217 <arnoldbuining@hotmail.com>
2017-11-04 18:58:24 +0100

    Fout weergave bij foutieve login

--- php/screens/login.php
+++ php/screens/login.php
@@ -1,9 +1,27 @@
-<div id="login_popup">
+<?php
+
+$loginErrorMsg = null;
+$popupOpened = null;
+if(!empty($_GET['login'])){
+    switch($_GET['login']){
+        case "wrongcredentials":
+            $loginErrorMsg="Je login is incorrect. Controleer je gebruikersnaam (email) en/of wachtwoord en probeer het opnieuw.";
+            $loginErrorMsg = "<p class='errormsg'>".$loginErrorMsg."</p>";
+            $popupOpened = "style='display:block;'";
+            break;
+        default:
+            break;
+    }
+}
+
+?>
+<div id="login_popup" <?=$popupOpened;?>>
     <div class="overlay" id="popup_overlay">
         <div class="popup">
             <div class="title">
                 <h2>Login</h2>
             </div>
+            <?=$loginErrorMsg;?>
             <div class="content">
                 <form action="<?= $config['Base_URL']; ?>/php/form_handler.php" method="post">
                     <table class="fancy_labels">
