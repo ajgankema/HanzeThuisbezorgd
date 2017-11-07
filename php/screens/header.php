a88ded9f6909019ab797185be01682942b29ddb8
Thomas van Strijland <thomasvanstrijland@gmail.com>
2017-11-07 01:16:08 +0100

    Zoekbalk fix
    Zoekbalk was ook op de account pagina te vinden. Staat nu alleen op de home-page.

--- php/screens/header.php
+++ php/screens/header.php
@@ -42,19 +42,4 @@
 
         </div>
     </div>
-    <div id="zoekpaneel">
-        <form>
-            <div id="custom-search-input">
-                <div class="input-group col-md-12">
-                    <input id="search_input" type="text" class="search-query form-control" placeholder="Adres of postcode..." />
-                    <span class="input-group-btn">
-                            <button class="btn btn-danger" type="button">
-                                <span class=" glyphicon glyphicon-search">
-                                </span>
-                            </button>
-                        </span>
-                </div>
-            </div>
-        </form>
-    </div>
 </header>
